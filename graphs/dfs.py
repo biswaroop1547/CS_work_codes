1ce249b0026cf82d8c056003d12749ef2b4a024a
biswaroop1547 <biswaroop08@gmail.com>
2021-03-25 06:15:38 +0530

    added topological sorting + dfs (fixed code)

--- graphs/dfs.py
+++ graphs/dfs.py
@@ -1,25 +1,37 @@
+# graph = {
+#     'a' : ['b', 'c', 'e'],
+#     'c' : ['a', 'e'],
+#     'e' : ['a', 'c', 'd'],
+#     'b' : ['a', 'd', 'f'],
+#     'd' : ['b', 'e', 'f'],
+#     'f' : ['b', 'd'],
+#     'h' : ['i', 'j'],
+#     'i' : ['h', 'j'],
+#     'j' : ['h', 'i'],
+# }
+
 graph = {
-    'a' : ['b', 'c', 'e'],
-    'c' : ['a', 'e'],
-    'e' : ['a', 'c', 'd'],
-    'b' : ['a', 'd', 'f'],
-    'd' : ['b', 'e', 'f'],
-    'f' : ['b', 'd'],
-    'h' : ['i', 'j'],
-    'i' : ['h', 'j'],
-    'j' : ['h', 'i'],
+    'g' : ['h'],
+    'a' : ['h', 'b'],
+    'h' : [],
+    'd' : ['b', 'e'],
+    'e' : ['f'],
+    'b' : ['c'],
+    'c' : ['f'],
+    'f' : [],
+    'i' : []
 }
 
 def dfs_visit(s, graph, parent = None):
     if parent is None:
         parent = {s : None}
-    finish_list = [s]
+    finish_list = []
     def dfs_visit_recurr(s, graph):
         for v in graph[s]:
             if v not in parent:
                 parent[v] = s
-                finish_list.append(v)
                 dfs_visit_recurr(v, graph)
+                finish_list.append(v)
         return (parent, finish_list)
 
     return dfs_visit_recurr(s, graph)
@@ -32,9 +44,11 @@ def dfs(vertex_list, graph):
         if v not in parent:
             parent[v] = None
             parent, path = dfs_visit(v, graph, parent)
-            paths.append(path)
+            path.append(v)
+            paths.extend(path)
             
     return paths, parent
 
 
-print(dfs(graph.keys(), graph)[1])
+print(dfs(graph.keys(), graph)[0][: : -1])
+# print(dfs_visit('d', graph))
