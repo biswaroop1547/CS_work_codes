d30bb48febf5141aefeb78d115caba71d6d06f54
biswaroop1547 <biswaroop08@gmail.com>
2021-03-25 05:45:18 +0530

    added dfs code (fixed for all vertex visit)

--- graphs/dfs.py
+++ graphs/dfs.py
@@ -4,17 +4,24 @@ graph = {
     'e' : ['a', 'c', 'd'],
     'b' : ['a', 'd', 'f'],
     'd' : ['b', 'e', 'f'],
-    'f' : ['b', 'd']
+    'f' : ['b', 'd'],
+    'h' : ['i', 'j'],
+    'i' : ['h', 'j'],
+    'j' : ['h', 'i'],
 }
 
-def dfs_visit(s, graph):
-    parent = {s : None}
+def dfs_visit(s, graph, parent = None):
+    if parent is None:
+        parent = {s : None}
+    finish_list = [s]
     def dfs_visit_recurr(s, graph):
         for v in graph[s]:
             if v not in parent:
                 parent[v] = s
+                print(v)
+                finish_list.append(v)
                 dfs_visit_recurr(v, graph)
-        return parent
+        return (parent, finish_list)
 
     return dfs_visit_recurr(s, graph)
 
@@ -25,8 +32,8 @@ def dfs(vertex_list, graph):
     for v in vertex_list:
         if v not in parent:
             parent[v] = None
-            paths.append(dfs_visit(v, graph))
+            paths.append(dfs_visit(v, graph, parent)[1])
     return paths
 
 
-print(dfs(['a', 'c'], graph))
+print(dfs(graph.keys(), graph))
